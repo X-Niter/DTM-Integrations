c81325f0d4c9644c3ecefa3a7b3fa2fdf0586cf7
X_Niter <drumsforlife1999@yahoo.com>
2022-08-16 00:18:39 -0500

    City world generation number changes

--- src/main/java/github/xniter/dtmintegrations/mixin/MixinCityWorldGenerator.java
+++ src/main/java/github/xniter/dtmintegrations/mixin/MixinCityWorldGenerator.java
@@ -1,23 +1,26 @@
 package github.xniter.dtmintegrations.mixin;
 
+import com.dhanantry.scapeandrunparasites.world.gen.structure.WorldGenStructure;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import nuparu.sevendaystomine.world.gen.CityWorldGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Mixin({CityWorldGenerator.class})
-public class MixinCityWorldGenerator implements ILateMixinLoader, IWorldGenerator {
+public class MixinCityWorldGenerator implements ILateMixinLoader {
 
 
 
@@ -27,10 +30,10 @@ public class MixinCityWorldGenerator implements ILateMixinLoader, IWorldGenerato
      */
     @Overwrite(remap = false)
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-        int blockX = chunkX * 16;
-        int blockZ = chunkZ * 16;
+//        int blockX = chunkX * 16;
+//        int blockZ = chunkZ * 16;
         if (world.provider.getDimension() == 0) {
-            this.generateOverworld(world, random, blockX, blockZ);
+            this.generateOverworld(world, random, chunkX, chunkZ);
         }
 
     }
@@ -41,15 +44,39 @@ public class MixinCityWorldGenerator implements ILateMixinLoader, IWorldGenerato
      */
     @Overwrite(remap = false)
     private void generateOverworld(World world, Random rand, int chunkX, int chunkZ) {
-        if (world.getWorldType() != WorldType.FLAT) {
-            if (chunkX % 16 == 0 && chunkZ % 16 == 0) {
-                int blockX = chunkX * 16;
-                int blockZ = chunkZ * 16;
-                BlockPos pos = new BlockPos(blockX, 64, blockZ);
-                world.getBiomeForCoordsBody(pos);
+
+        int worldY = world.getHeight();
+
+        int x = chunkX * 16 + rand.nextInt(15);
+        int z = chunkZ * 16 + rand.nextInt(15);
+
+        Block blockAt = world.getBlockState(new BlockPos(x, worldY, z)).getBlock();
+        for(boolean foundGround = false; !foundGround && worldY-- >= 0; foundGround = blockAt == Blocks.DIRT || blockAt == Blocks.GRASS) {
+            blockAt = world.getBlockState(new BlockPos(x, worldY, z)).getBlock();
+        }
+
+        int y = calculateHeight(world, x, z, blockAt);
+
+        BlockPos pos = new BlockPos(x, y, z);
+
+        if (chunkX % 64 == 0 && chunkZ % 64 == 0) {
+            if (world.getWorldType() != WorldType.FLAT) {
+                world.provider.getBiomeForCoords(pos);
                 world.getChunk(pos);
             }
         }
+    }
+
+
+    private static int calculateHeight(World world, int x, int z, Block topBlock) {
+        int y = world.getHeight();
+
+        Block block;
+        for(boolean flag = false; !flag && y-- >= 0; flag = block == topBlock) {
+            block = world.getBlockState(new BlockPos(x, y, z)).getBlock();
+        }
+
+        return y;
     }
 
 
