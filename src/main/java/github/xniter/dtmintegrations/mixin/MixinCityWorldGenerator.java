9c0b8743ddf0d878d97e839a18e18ce0795c5507
X_Niter <drumsforlife1999@yahoo.com>
2022-08-16 19:44:29 -0500

    Revert testing code

--- src/main/java/github/xniter/dtmintegrations/mixin/MixinCityWorldGenerator.java
+++ src/main/java/github/xniter/dtmintegrations/mixin/MixinCityWorldGenerator.java
@@ -30,58 +30,23 @@ public class MixinCityWorldGenerator implements ILateMixinLoader {
      */
     @Overwrite(remap = false)
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-//        int blockX = chunkX * 16;
-//        int blockZ = chunkZ * 16;
         if (world.provider.getDimension() == 0) {
             this.generateOverworld(world, random, chunkX, chunkZ);
         }
-
     }
 
-    /**
-     * @author X_Niter
-     * @reason f
-     */
-    @Overwrite(remap = false)
+    @Shadow
     private void generateOverworld(World world, Random rand, int chunkX, int chunkZ) {
-
-        int worldY = world.getHeight();
-
-        int x = chunkX * 16 + rand.nextInt(15);
-        int z = chunkZ * 16 + rand.nextInt(15);
-
-        Block blockAt = world.getBlockState(new BlockPos(x, worldY, z)).getBlock();
-        for(boolean foundGround = false; !foundGround && worldY-- >= 0; foundGround = blockAt == Blocks.DIRT || blockAt == Blocks.GRASS) {
-            blockAt = world.getBlockState(new BlockPos(x, worldY, z)).getBlock();
-        }
-
-        int y = calculateHeight(world, x, z, blockAt);
-
-        BlockPos pos = new BlockPos(x, y, z);
-
-        if (chunkX % 64 == 0 && chunkZ % 64 == 0) {
-            if (world.getWorldType() != WorldType.FLAT) {
-                world.provider.getBiomeForCoords(pos);
+        if (world.getWorldType() != WorldType.FLAT) {
+            if (chunkX % 64 == 0 && chunkZ % 64 == 0) {
+                int blockX = chunkX * 16;
+                int blockZ = chunkZ * 16;
+                BlockPos pos = new BlockPos(blockX, 64, blockZ);
+                world.getBiomeForCoordsBody(pos);
                 world.getChunk(pos);
             }
         }
     }
-
-
-    private static int calculateHeight(World world, int x, int z, Block topBlock) {
-        int y = world.getHeight();
-
-        Block block;
-        for(boolean flag = false; !flag && y-- >= 0; flag = block == topBlock) {
-            block = world.getBlockState(new BlockPos(x, y, z)).getBlock();
-        }
-
-        return y;
-    }
-
-
-
-
 
     @Override
     public List<String> getMixinConfigs()
