c26b9d7e92c4d479018d99c08026013571611225
X_Niter <drumsforlife1999@yahoo.com>
2022-08-21 14:57:35 -0500

    Update config change, TODO list added

--- src/main/java/github/xniter/dtmintegrations/DTMIntegrations.java
+++ src/main/java/github/xniter/dtmintegrations/DTMIntegrations.java
@@ -1,25 +1,13 @@
 package github.xniter.dtmintegrations;
 
-import github.xniter.dtmintegrations.config.DTMIConfig;
+import github.xniter.dtmintegrations.handlers.config.ConfigHandler;
+import github.xniter.dtmintegrations.handlers.FMLEvents;
 import github.xniter.dtmintegrations.features.FMLAutoConfig;
-import github.xniter.dtmintegrations.integration.IntegrationHelper;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import nuparu.sevendaystomine.SevenDaysToMine;
-import nuparu.sevendaystomine.util.Utils;
-import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
 
 @Mod(
         modid = DTMIntegrations.MOD_ID,
@@ -29,10 +17,11 @@ import java.util.Scanner;
 )
 public class DTMIntegrations {
     private static final int CONFIG_VERSION = 2;
-
     public static final String MOD_ID = "dtmintegrations";
     public static final String MOD_NAME = "DTM Integrations";
     public static final String VERSION = "1.3.0";
+
+    public static File config;
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
@@ -46,66 +35,26 @@ public class DTMIntegrations {
      */
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-        Logger log = event.getModLog();
-        File modDir = new File(event.getModConfigurationDirectory(), "DTMIntegrations");
-        modDir.mkdirs();
-
-        // Check if config is outdated and needs to be deleted
-        boolean configOutdated;
-        File configVersionFile = new File(modDir, "config_version");
-        if (configVersionFile.exists()) {
-            try (FileReader reader = new FileReader(configVersionFile); Scanner scanner = new Scanner(reader)) {
-                try {
-                    configOutdated = scanner.nextInt() != CONFIG_VERSION;
-                } catch (NumberFormatException e) {
-                    configOutdated = true;
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            configOutdated = true;
-        }
-
-        if (configOutdated) {
-            File configFile = new File(modDir, "DTMIntegrations.cfg");
-            if (configFile.exists()) {
-                log.info("Regenerating outdated config");
-                configFile.delete();
-            }
-            try (FileWriter writer = new FileWriter(configVersionFile)) {
-                writer.write(String.valueOf(CONFIG_VERSION));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        ConfigManager.sync("dtmintegrations", Config.Type.INSTANCE);
+        ConfigHandler.registerConfig(event);
 
         // Register event listeners
-        MinecraftForge.EVENT_BUS.register(DTMIConfig.class);
+        MinecraftForge.EVENT_BUS.register(ConfigHandler.class);
         MinecraftForge.EVENT_BUS.register(FMLAutoConfig.class);
-
-        if (IntegrationHelper.isLootrLoaded()) {
-
-            if (!noobanidus.mods.lootr.config.ConfigManager.CONVERT_ALL_LOOTABLES_EXCEPT_BELOW) {
-
-                Utils.getLogger().error("LOOTR is loaded but [CONVERT_ALL_LOOTABLES_EXCEPT_BELOW] is false, EXPECT invisible cars");
+        MinecraftForge.EVENT_BUS.register(FMLEvents.class);
 
 
-            } else {
-                Arrays.stream(SevenDaysToMine.BLOCKS).forEach(block -> {
-                    if (block instanceof IInventory) {
-                        ResourceLocation blockResource = block.getRegistryName();
-                        noobanidus.mods.lootr.config.ConfigManager.getAdditionalChests().add(blockResource);
-                    }
-                });
-            }
-
-        }
 
 
     }
+
+    /* TODO
+        Pipe recipe (recipe?) [1 forged iron in inventory(1 pipe IRL), or 10 iron scrap and clay in forge (1 Pipe IRL)]
+        Claw Hammer recipe (Recipe Book?)
+        Wrench recipe (Recipe Book?)
+        Pipe weapons, comes with perk in irl game
+        Airdrop (Wheres the fucking bundles bro?)
+        Drinks give tough as nails thirst values
+     */
 
 //    /**
 //     * This is the second initialization event. Register custom recipes
