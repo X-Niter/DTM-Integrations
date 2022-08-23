ee8128de5c9031e8657ef6e4939e9051097c6e4b
X_Niter <drumsforlife1999@yahoo.com>
2022-08-22 19:04:55 -0500

    So many config options

--- src/main/java/github/xniter/dtmintegrations/handlers/config/ConfigGetter.java
+++ src/main/java/github/xniter/dtmintegrations/handlers/config/ConfigGetter.java
@@ -31,4 +31,43 @@ public class ConfigGetter extends ConfigHandler{
     public static boolean getFocusedWolfHorde(){
         return FOCUSED_WOLFHORDE;
     }
+    public static boolean getUseLangConfig(){
+        return USE_LANG_CONFIG;
+    }
+
+    public static String getAirdropDespawnMessage(){
+        return AIRDROP_DESPAWNED_MESSAGE;
+    }
+
+    public static int getAirdropMaxHeight(){
+        return AIRDROP_MAX_HEIGHT;
+    }
+
+     public static boolean getAirdropChatMessageEnabled(){
+         return AIRDROP_CHAT_MESSAGE_ENABLED;
+     }
+
+    public static boolean getAirdropChatMessageExactLocation(){
+        return AIRDROP_CHAT_MESSAGE_EXACT_LOCATION;
+    }
+
+    public static int getAirdropChatMessageGeneralLocation(){
+        return AIRDROP_CHAT_MESSAGE_GENERAL_LOCATION;
+    }
+
+    public static int getAirdropDistanceFromPlayer(){
+        return AIRDROP_DISTANCE_FROM_PLAYER;
+    }
+
+    public static double getAirdropFallingSpeed(){
+        return AIRDROP_FALL_SPEED;
+    }
+
+    public static int getAirdropSmokeTime(){
+        return AIRDROP_SMOKE_TIME;
+    }
+
+    public static boolean getAirdropRealisticFalling(){
+        return AIRDROP_REALISTIC_FALLING;
+    }
 }
--- src/main/java/github/xniter/dtmintegrations/handlers/config/ConfigHandler.java
+++ src/main/java/github/xniter/dtmintegrations/handlers/config/ConfigHandler.java
@@ -1,9 +1,6 @@
 package github.xniter.dtmintegrations.handlers.config;
 
-import com.google.common.base.Utf8;
 import github.xniter.dtmintegrations.DTMIntegrations;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Configuration;
@@ -36,6 +33,25 @@ public class ConfigHandler {
 
     protected static boolean FORCEFUL_FOCUSED_WOLFHORDE = false;
 
+    protected static boolean USE_LANG_CONFIG = true;
+
+    protected static String AIRDROP_DESPAWNED_MESSAGE = "Airdrop in world %1$s, around X:%2$s Y:%3$s Z:%4$s has de-spawned!";
+
+    protected static int AIRDROP_MAX_HEIGHT = 256;
+
+    protected static boolean AIRDROP_CHAT_MESSAGE_ENABLED = true;
+
+    protected static boolean AIRDROP_CHAT_MESSAGE_EXACT_LOCATION = false;
+
+    protected static int AIRDROP_CHAT_MESSAGE_GENERAL_LOCATION = 32;
+    protected static int AIRDROP_DISTANCE_FROM_PLAYER = 128;
+
+    protected static double AIRDROP_FALL_SPEED = 0.1;
+
+    protected static int AIRDROP_SMOKE_TIME = 60;
+
+    protected static boolean AIRDROP_REALISTIC_FALLING = false;
+
     public static void init(File file){
         config = new Configuration(file);
         String category;
@@ -48,7 +64,7 @@ public class ConfigHandler {
         FORGE_OPT = config.getBoolean("Forge Optimization", category, true, "Automatically sets Forges config options too the most optimal setting for game/modpack healthiness/performance");
 
         category = "7 Days To Mine";
-        config.addCustomCategoryComment(category, "THE Days To Mine Integration that smashes the stupid out of 7 Days To Mine XD (That's right I said it, don't judge and just enjoy!)");
+        config.addCustomCategoryComment(category, "General Config setting for Seven Days To Mine changes");
         BM_SLEEPING = config.getBoolean("BloodMoon Sleeping", category, false, "[Default: FALSE(No sleeping allowed)]\n[TRUE = Sleeping allowed during Blood Moon]\n[FALSE = Sleeping not allowed during Blood Moon]\nCan players sleep through a Blood Moon?");
         STREET_GEN_ATTEMPTS = config.getInt("Street Gen Attempts", category, 3, 1, 25, "[WARNING: Higher numbers severally decreases performance :USE CAUTION:]\n[SAFE OPTION: 1-10]\n[UNSAFE OPTION: 11+]\n7DTM Originally had this set to 8192 times, THAT'S A LOT, no wonder generation was so heavy!\nWith this option being controllable now, the generation is still heavy but it's much better when this is left at default or lower.\nYou can indeed set this to 1 to generate the roads at a lower increment, meaning the generation will happen in smaller bits but faster, where as the default 3 means that the road generation will repeat three times to potentially generate a longer road instead of a smaller incremented generation.\n\nMax attempts to generate streets?");
         CHANGE_TORCHES = config.getBoolean("Change Torches", category, true, "Can Seven Days To Mine change torches? [Set to False if any other mods installed already make modifications to vanilla torches]");
@@ -58,6 +74,24 @@ public class ConfigHandler {
         FORCEFUL_FOCUSED_BLOODMOON = config.getBoolean("Forceful Focused BloodMoon", category, false, "During a BloodMoon, ALL mobs that are not apart of the BloodMoon event will be removed forcefully from existence until the event is over");
         FORCEFUL_FOCUSED_WOLFHORDE = config.getBoolean("Forceful Focused WolfHorde", category, false, "During a WolfHorde, ALL mobs that are not apart of the WolfHorde event will be removed forcefully from existence until the event is over");
 
+        category = "7 Days To Mine Airdrops";
+        config.addCustomCategoryComment(category, "More in depth, fine controls over Airdrops");
+
+        AIRDROP_MAX_HEIGHT = config.getInt("Max Drop Height", category, 256, 50, 256, "[DEFAULT: 256]\nThe max height the Airdrop can drop from");
+        AIRDROP_CHAT_MESSAGE_ENABLED = config.getBoolean("Chat Notification", category, true, "[DEFAULT: TRUE(Enabled)]\nEnable/Disable Airdrop chat notifications");
+        AIRDROP_CHAT_MESSAGE_EXACT_LOCATION = config.getBoolean("Notification Exact Location", category, false, "[DEFAULT: FALSE(disabled)]\nIf FALSE, then the below general location will be used\nDoes the Airdrop message in chat give the exact location of the Airdrop?");
+        AIRDROP_CHAT_MESSAGE_GENERAL_LOCATION = config.getInt("Notification General Location", category, 32, 1, 256, "[DEFAULT 32 Block Range]\nThe range of the general location given in chat\nThis range controls how RANDOMLY far off the location given in chat can be, so 32 would mean that Airdrop locations given in chat is within 32 blocks of range from the actual Airdrop location\nThe equation used is [Pos x + random number between 0 and (Number you set) - random number between 0 and (Number you set)], the same equation applies to the z coords as well, x was just used for the showing of equation.");
+        AIRDROP_DISTANCE_FROM_PLAYER = config.getInt("Drop Distance From Player", category, 128, 16, 256, "[DEFAULT: 128 Blocks]\nThe max range in blocks that Airdrops can be dropped around a player");
+        AIRDROP_FALL_SPEED = config.getInt("Fall speed", category, 1, 0, 50, "[DEFAULT: 1]\n[0 is Seven Days To Mine Default VERY SLOW falling]\nHow fast the Airdrop falls, higher number is faster falling.");
+        AIRDROP_SMOKE_TIME = config.getInt("Smoke Time", category, 60, 1, 36000, "[DEFAULT 60 seconds]\n[MAX is 10 Hours aka 36000 seconds]\nHow many seconds will the Airdrop shoot smoke out the top of the box");
+        AIRDROP_REALISTIC_FALLING = config.getBoolean("Realistic Falling", category, false, "[DEFAULT: FALSE]\nEnable/Disable realistic ANGLE falling Airdrop");
+
+
+        category = "Language";
+        config.addCustomCategoryComment(category, "Message Configurations, I'm not a big fan of lang files, so here you go, you can have your very own set of lang.\nLang File PR's are still welcomed and accepted if that is your preference!");
+        USE_LANG_CONFIG = config.getBoolean("Use Language Config", category, true, "[DEFAULT: TRUE(Enabled)]\nEnable/Disable using this config for configuring messages/lang translations");
+        AIRDROP_DESPAWNED_MESSAGE = config.getString("Airdrop Despawn Message", category, "Airdrop in world %1$s, around X:%2$s Y:%3$s Z:%4$s has de-spawned!", "The message sent to chat when the AirDrop de-spawns after sitting for too long.");
+
 
         config.save();
     }
@@ -66,6 +100,11 @@ public class ConfigHandler {
         DTMIntegrations.config = new File(event.getModConfigurationDirectory() + "/" + "DTMIntegrations");
         DTMIntegrations.config.mkdir();
         init(new File(DTMIntegrations.config.getPath(), "DTMIntegrations" + ".cfg"));
+
+        if (config.hasChanged()) {
+            config.save();
+            config.load();
+        }
     }
 
 
